63bbad997cd9fdb948c739e50dac91515c57d69a
slyderyg <kprokofev88@icloud.com>
2023-10-08 07:41:33 +0300

    clean project

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Candeller',
+  description: 'Candeller - Candle Shop',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
