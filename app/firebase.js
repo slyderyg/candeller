a21b453dd0aac4147c1aa44c0ccca9dc3cb799d3
slyderyg <kprokofev88@icloud.com>
2023-10-09 06:45:01 +0300

    install firebase

---
+++ app/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9cH8bojJ1lWxwvo0rkaB75VDWoZRNAjw",
+    authDomain: "candeller.firebaseapp.com",
+    projectId: "candeller",
+    storageBucket: "candeller.appspot.com",
+    messagingSenderId: "469783033127",
+    appId: "1:469783033127:web:379d77af0cc58cf8196502"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
