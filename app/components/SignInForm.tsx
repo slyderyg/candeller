b662b39c92af3533c9cc6b51b3d0aa3c094d1321
slyderyg <kprokofev88@icloud.com>
2023-10-13 20:20:17 +0300

    modal finished

--- app/components/SignInForm.tsx
+++ app/components/SignInForm.tsx
@@ -1,22 +1,34 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 interface ModalProps {
   isSignInHandler: (isSignIn: boolean) => void;
 }
 
 const SignInForm: FC<ModalProps> = ({ isSignInHandler }) => {
+
+  const [modalEmail, setModalEmail] = useState('');
+  const [modalPassword, setModalPassword] = useState('');
+
+  const handleModalEmail = (e:ChangeEvent<HTMLInputElement>): void => {
+    setModalEmail(e.target.value)
+  }
+  
+  const handleModalPassword = (e:ChangeEvent<HTMLInputElement>): void => {
+    setModalPassword(e.target.value)
+  }
+
   return (
     <div className='form'>
 
         <div className='form__group'>
-            <input className='form__input' type='email' placeholder=''/>
+            <input className='form__input' type='email' placeholder='' value={modalEmail} onChange={handleModalEmail}/>
             <label className='form__label' htmlFor="">EMAIL</label>
         </div>
 
         <div className='form__group'>
-            <input className='form__input' type='password' placeholder=''/>
+            <input className='form__input' type='password' placeholder='' value={modalPassword} onChange={handleModalPassword}/>
             <label className='form__label' htmlFor="">PASSWORD</label>
         </div>
 
