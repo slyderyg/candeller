488cce4e074c9d8ec080db0b0e8549981ee3dc19
slyderyg <kprokofev88@icloud.com>
2023-10-17 19:42:50 +0300

    sign up form validation finished

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -12,9 +12,9 @@ const SignUpForm: FC<ModalProps> = ({ isSignInHandler }) => {
 
 const { createUser } = useContext(Context);
 
-const signUpEmail = useInput('');
-const signUpPassword = useInput('');
-const signUpPasswordRepeat = useInput('');
+const signUpEmail = useInput("", "email");
+const signUpPassword = useInput("", "password");
+const signUpPasswordRepeat = useInput("", "password");
 
 
 const handleClick = (): void => {
@@ -26,24 +26,25 @@ const handleClick = (): void => {
     <div className='form'>
 
         <div className='form__group'>
-            <input className='form__input' type='email' placeholder='' value={signUpEmail.value} onChange={signUpEmail.handleInput}/>
-            <label className='form__label' htmlFor="">EMAIL</label>
+            <input className='form__input' type='email' placeholder='' value={signUpEmail.value} onChange={signUpEmail.onChange} onBlur={signUpEmail.onBlur}/>
+            <label className={signUpEmail.emailError? 'form__label__error' : 'form__label'} htmlFor="">{signUpEmail.emailError? (signUpEmail.emailError):('EMAIL')}</label>
         </div>
 
         <div className='form__group'>
-            <input className='form__input' type='password' placeholder='' value={signUpPassword.value} onChange={signUpPassword.handleInput}/>
-            <label className='form__label' htmlFor="">PASSWORD</label>
+            <input className='form__input' type='password' placeholder='' value={signUpPassword.value} onChange={signUpPassword.onChange} onBlur={signUpPassword.onBlur}/>
+            <label className={signUpPassword.passwordError? 'form__label__error' : 'form__label'} htmlFor="">{signUpPassword.passwordError? (signUpPassword.passwordError):('PASSWORD')}</label>
         </div>
 
         <div className='form__group'>
-            <input className='form__input' type='password' placeholder='' value={signUpPasswordRepeat.value} onChange={signUpPasswordRepeat.handleInput}/>
-            <label className='form__label' htmlFor="">REPEAT PASSWORD</label>
+            <input className='form__input' type='password' placeholder='' value={signUpPasswordRepeat.value} onChange={signUpPasswordRepeat.onChange} onBlur={signUpPasswordRepeat.onBlur}/>
+            <label className={(signUpPasswordRepeat.value !== signUpPassword.value)&&signUpPassword.isDirty? 'form__label__error' : 'form__label'} htmlFor="">{(signUpPasswordRepeat.value !== signUpPassword.value)&&signUpPassword.isDirty? ('password dismatch'):('REPEAT PASSWORD')}</label>
         </div>
 
         <div className='button__group'>
-        <button className='form__button' onClick={handleClick}>SIGN UP</button>
+        <button className={signUpEmail.emailError || signUpPassword.passwordError || (signUpPasswordRepeat.value !== signUpPassword.value) || (signUpPassword.value.length<6)? 'form__button__error': 'form__button'} onClick={handleClick}>SIGN UP</button>
         <button className='form__reg__button' onClick={()=>{isSignInHandler(true)}}>SIGN IN</button>
         </div>
+        
     </div>
   )
 }
--- app/utils/useInput.ts
+++ app/utils/useInput.ts
@@ -1,14 +1,46 @@
-import { useState, ChangeEvent } from "react"
+import { useState, useEffect, ChangeEvent } from "react"
 
-export const useInput = (initialValue: string) => {
+export const useInput = (initialValue: string, validationType: string) => {
     const [value, setValue] = useState(initialValue);
+    const [emailError, setemailError] = useState('');
+    const [passwordError, setPasswordError] = useState('')
+    const [isDirty, setDirty] = useState(false);
 
-    const handleInput = (e:ChangeEvent<HTMLInputElement>): void => {
-        setValue(e.target.value);
-    }
+    const onChange = (e:ChangeEvent<HTMLInputElement>): void  => {
+      setValue(e.target.value);
+    };
+
+    const onBlur = (e:ChangeEvent<HTMLInputElement>): void  => {
+      setDirty(true);
+    };
+
+    useEffect(() => {
+      switch (validationType) {
+        case "email":
+          const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          !re.test(value) && isDirty
+            ? setemailError('invalid email format')
+            : setemailError('');
+          break;
+        case "password":
+          value.length < 6 && isDirty
+            ? setPasswordError("minimum password length 6 characters")
+            : setPasswordError('');
+          break;
+      }
+    }, [value, isDirty]);
 
     return {
-        value,
-        handleInput
-    }
-}
+      value,
+      onChange,
+      emailError,
+      onBlur,
+      passwordError,
+      isDirty
+    };
+  };
+
+  
+
+
+
